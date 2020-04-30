c2fe65a1227e46b96bb09d0fe36af9dfd41a4de5
raunaqjain <jraunaq18@gmail.com>
2020-04-29 21:58:24 -0400

    Add sentence selection training ipynb

--- sentence_selection/data_creation.py
+++ sentence_selection/data_creation.py
@@ -7,7 +7,7 @@ from utils import parse_text, lines_to_items
 import argparse
 
 
-def parser(index, iid, verifiable, claim, label, article):
+def parseRow(index, iid, verifiable, claim, label, article):
     data = []
     article_title = article['id_string']
     article_text = article['lines'][0]
@@ -28,7 +28,7 @@ def parser(index, iid, verifiable, claim, label, article):
         else:
             temp['label'] = False
             
-        if not temp['label'] and random.random() <= 0.6:
+        if not temp['label'] and random.random() <= 1 - sample_prob:
             continue
             
         temp['sentence'] = line['sentences']
@@ -56,7 +56,7 @@ def create_training_data(docs):
         for key, row in df.iterrows():
             iid, verifiable, claim, label, articles = row
             for article in articles:
-                data = parser(key, iid, verifiable, claim, label, row['articles'][article])
+                data = parseRow(key, iid, verifiable, claim, label, row['articles'][article])
                 training_data.extend(data)
         print (i, doc)
     pd.DataFrame(training_data).to_csv(output_folder, index=False)
@@ -67,15 +67,18 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Formulate data for sentence model")
     parser.add_argument("--input", help="path to input folder")
     parser.add_argument("--output", help="path to output file")
+    parser.add_argument("--sample_prob", help="probability of choosing false labels", type=float, default=1.0)
     args = parser.parse_args()
 
     input_folder = args.input
     output_folder = args.output
+    sample_prob = args.sample_prob
+    print(sample_prob)
     # DOC_PATH = "doc_retrieval/document_retrieved_v2/"
     # SENTENCE_PATH = "sentence_training_data/v2/"
 
     list_of_docs = sorted(os.listdir(input_folder))
-    print (list_of_docs)
+    print(list_of_docs)
     create_training_data(list_of_docs)
 
 
